62812945d753168c4d6a4880bb467fa0cf19ea12
hexuustc <hexuustc@mail.ustc.edu.cn>
2021-09-26 15:14:58 +0800

    初次提交版本

--- report.py
+++ report.py
@@ -1,135 +1,57 @@
 # encoding=utf8
 import requests
-import json
-import time
 import datetime
 import pytz
-import re
-import sys
 import argparse
-from bs4 import BeautifulSoup
-from aip import AipOcr
- 
-# 定义常量
-APP_ID = '24565086'
-API_KEY = 'tgkntU18fTayWntdivP5jv4G'
-SECRET_KEY = '1GfGV1T0f6AblLSlLTs4KON21IFPIKlH'
+import random
 
 class Report(object):
-    def __init__(self, stuid, password, data_path):
-        self.stuid = stuid
+    def __init__(self, user, password):
+        self.user = user
         self.password = password
-        self.data_path = data_path
 
     def report(self):
-        loginsuccess = False
-        retrycount = 2
-        while (not loginsuccess) and retrycount:
-            session = self.login()
-            cookies = session.cookies
-            getform = session.get("https://weixine.ustc.edu.cn/2020")
-            retrycount = retrycount - 1
-            if getform.url != "https://weixine.ustc.edu.cn/2020/home":
-                print("Login Failed! Retrying...")
-            else:
-                print("Login Successful!")
-                loginsuccess = True
-        if not loginsuccess:
-            return False
-        data = getform.text
-        data = data.encode('ascii','ignore').decode('utf-8','ignore')
-        soup = BeautifulSoup(data, 'html.parser')
-        token = soup.find("input", {"name": "_token"})['value']
-
-        with open(self.data_path, "rb") as f:
-            data = f.read()
-            data = json.loads(data)
-            data["_token"]=token
-
-        headers = {
-            'authority': 'weixine.ustc.edu.cn',
-            'origin': 'https://weixine.ustc.edu.cn',
-            'upgrade-insecure-requests': '1',
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'referer': 'https://weixine.ustc.edu.cn/2020/home',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            'Connection': 'close',
-            'cookie': "PHPSESSID=" + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
-        }
-
-        url = "https://weixine.ustc.edu.cn/2020/daliy_report"
-        resp=session.post(url, data=data, headers=headers)
-        data = session.get("https://weixine.ustc.edu.cn/2020").text
-        soup = BeautifulSoup(data, 'html.parser')
-        pattern = re.compile("202[0-9]-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
-        token = soup.find(
-            "span", {"style": "position: relative; top: 5px; color: #666;"})
-        flag = False
-        if pattern.search(token.text) is not None:
-            date = pattern.search(token.text).group()
-            print("Latest report: " + date)
-            date = date + " +0800"
-            reporttime = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
-            timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
-            delta = timenow - reporttime
-            print("{} second(s) before.".format(delta.seconds))
-            if delta.seconds < 120:
-                flag = True
-        if flag == False:
-            print("Report FAILED!")
+        url = "http://wannengjun666.xyz/mi.php"
+        timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+        print(timenow.hour)
+        if timenow.hour<=8 and timenow.hour>=7:
+            step = random.randint(1000,1500)
+        elif timenow.hour<=12 and timenow.hour>=11:
+            step = random.randint(2000,2500)
+        elif timenow.hour<=18 and timenow.hour>=17:
+            step = random.randint(4000,4500) 
+        elif timenow.hour<=23 and timenow.hour>=22:
+            step = random.randint(6000,7000) 
         else:
-            print("Report SUCCESSFUL!")
-        return flag
-
-    def login(self):
-        url = "https://passport.ustc.edu.cn/login?service=http%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
-        
-        session = requests.Session()
-
-        client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
-        image1 = session.get('https://passport.ustc.edu.cn/validatecode.jsp?type=login')
-        image = image1.content
-       
-        res=client.basicGeneral(image);
-        for item in res['words_result']:
-            number = item['words']
-        getform = session.get(url)
-        data = getform.text
-        data = data.encode('ascii','ignore').decode('utf-8','ignore')
-        soup = BeautifulSoup(data, 'html.parser')
-        token = soup.find("input", {"name": "CAS_LT"})['value']
+            step = random.randint(1000,2000)
         data = {
-            'model': 'uplogin.jsp',
-            'service': 'https://weixine.ustc.edu.cn/2020/caslogin',
-            'username': self.stuid,
-            'password': str(self.password),
-            'warn': '',
-            'showCode': '1',
-            'button': '',
-            'LT': number,
-            'CAS_LT': token
+            'user':self.user,
+            'password':self.password,
+            'step':step
         }
-        session.post(url, data=data)
-
-        print("login...")
-        return session
-
+        ret = requests.post(url,data=data)
+        status = ret.status_code
+        print(status)
+        if status==200:
+            print("Sport Success!")
+            return True
+        else:
+            print("Sport Failed!")
+            return False
+        return False
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='URC nCov auto report script.')
-    parser.add_argument('data_path', help='path to your own data used for post method', type=str)
-    parser.add_argument('stuid', help='your student number', type=str)
-    parser.add_argument('password', help='your CAS password', type=str)
+    parser = argparse.ArgumentParser(description='weixin auto sport script.')
+    parser.add_argument('user', help='your user name', type=str)
+    parser.add_argument('password', help='your password', type=str)
     args = parser.parse_args()
-    autorepoter = Report(stuid=args.stuid, password=args.password, data_path=args.data_path)
+    autorepoter = Report(user=args.user, password=args.password)
     count = 2
     while count != 0:
         ret = autorepoter.report()
         if ret != False:
             break
-        print("Report Failed, retry...")
+        print("Sport Failed, retry...")
         count = count - 1
     if count != 0:
         exit(0)
